a7b5317b05291be8b3c6e0a705d3c9e8b921785e
Sandip Thapa <sandip.thapa543@gmail.com>
2019-09-24 11:14:06 +0545

    Initial commit

---
+++ app/src/main/java/com/first/firstapp/DashBoardActivity.java
@@ -0,0 +1,48 @@
+package com.first.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DashBoardActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnAdd, btnSub, btnCtof;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+
+        btnAdd=findViewById(R.id.btnAdd);
+        btnSub=findViewById(R.id.btnSub);
+        btnCtof=findViewById(R.id.btnctof);
+
+
+        btnAdd.setOnClickListener(this);
+        btnSub.setOnClickListener(this);
+        btnCtof.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId()== (R.id.btnAdd)){
+
+            Intent intent =new Intent(DashBoardActivity.this,MainActivity.class);
+            startActivity(intent);
+
+            if(v.getId()==(R.id.btnSub)){
+
+                Intent ints =new Intent(DashBoardActivity.this,MainActivity.class);
+                startActivity(ints);
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/first/firstapp/MainActivity.java
@@ -0,0 +1,56 @@
+package com.first.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText FirstNo, SecondNo;
+    private Button btnAdd, btnSub;
+    private TextView textview;
+    int result, Fno, Sno,r;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirstNo = findViewById(R.id.firsttxt);
+        SecondNo = findViewById(R.id.secondtxt);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnSub = findViewById(R.id.btnSub);
+        textview  = findViewById(R.id.tview);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fno = Integer.parseInt(FirstNo.getText().toString());
+                Sno = Integer.parseInt(SecondNo.getText().toString());
+                result = Fno + Sno;
+
+                Toast.makeText(MainActivity.this, "Sum is " + result, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fno = Integer.parseInt(FirstNo.getText().toString());
+                Sno = Integer.parseInt(SecondNo.getText().toString());
+                r = Integer.parseInt(textview.getText().toString());
+
+                r=Fno-Sno;
+                setContentView(r);
+
+
+            }
+        });
+
+
+    }
+}
